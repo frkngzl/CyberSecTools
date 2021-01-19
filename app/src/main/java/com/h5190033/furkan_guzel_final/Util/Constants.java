73138446e4b946e29a8eb45358a8cab6014d37a5
BaySamuraii <teslasbulb@gmail.com>
2021-01-19 21:09:30 +0200

    ilk commit

--- app/src/main/java/com/h5190033/furkan_guzel_final/Util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.h5190033.furkan_guzel_final.Util;
-
-public class Constants {
-    //Constants fonksiyonu için hazırlanmış dosya.
-    public static String TIKLANAN_ARAC_BASLIK = "tiklananArac";
-    //tiklananArac değeri tanımlanmış ve TIKLANAN_ARAC_BASLIK ile eşleştirilmiş.
-    public static final String BASE_URL = "https://raw.githubusercontent.com/frkngzl/H5190033FurkanGuzel/master/";
-    //Gerekli şeylerin çekilmesi için BASE_URL belirlenmiş.
-}
